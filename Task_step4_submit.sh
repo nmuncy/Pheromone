f8eede6837f3cb049194ef7dc0502a1d0ca09ef5
nmuncy <pingamormn@gmail.com>
2019-10-30 15:37:42 -0600

    starting group-level script

---
+++ Task_step4_submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+
+
+workDir=~/compute/WiscoPhero
+slurmDir=${workDir}/derivatives/Slurm_out
+time=`date '+%Y_%m_%d-%H_%M_%S'`
+outDir=${slurmDir}/TS4_${time}
+
+mkdir -p $outDir
+
+
+sbatch \
+-o ${outDir}/output_TS4.txt \
+-e ${outDir}/error_TS4.txt \
+Task_step4_grpAnalysis.sh
